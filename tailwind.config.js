61dcf7c4fc247bf47e45a1a8771a4b3ab1bce612
Yasir Gaji <gajiyasir@gmail.com>
2022-08-14 16:43:47 +0100

    did basic tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js, ts, jsx, tsx}',
+    './components/**/*.{js, ts, jsx, tsx}',
+  ],
+  theme: {
+    letterSpacing: {
+      tight: '-0.015em',
+    },
+    extend: {
+      height: {
+        'half-screen': '50vh',
+      },
+    },
+  },
+  plugins: [],
+};
