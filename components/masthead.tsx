16ed5c75f91db2fcd880bcd6bcef0b4bea2ebe5e
Yasir Gaji <gajiyasir@gmail.com>
2022-08-14 21:10:05 +0100

    break

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -4,7 +4,17 @@ import Image from 'next/image';
 const Masthead: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="p-12 font-bold">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover"
+      >
+        <source src="/assets/" type="video/mp4; codecs=hvc1" />
+        <source src="/assets/" type="video/webm; codecs=vp9" />
+      </video>
+      <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">webCourt</h1>
         <h2 className="mb-2 text-2xl xl:text3xl tracking-tight">
           <span>Web Development</span> <span>Done Right</span>
