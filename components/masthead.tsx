7dad63e614e75b59b893e45764ca205fc4167bef
Yasir Gaji <gajiyasir@gmail.com>
2022-08-16 15:59:26 +0100

    error refresh

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -15,15 +15,17 @@ const Masthead: React.FC = () => {
         <source src="/assets/wc-bg2.mp4" type="video/webm; codecs=vp9" />
       </video>
       <div className={'flex-grow-0 pt-10 transition-opacity duration-1000'}>
-        <Image
+        {/* <Image
           src="/assets/logo.svg"
           width={198 / 3}
           height={214 / 3}
           alt="logo"
-        />
+        /> */}
+
+        <h1 className="mb-6 text-4xl xl:text-5xl">XenturyLens</h1>
       </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
-        <h1 className="mb-6 text-4xl xl:text-5xl">webCourt</h1>
+        <h1 className="mb-6 text-4xl xl:text-5xl">XenturyLens</h1>
         <h2 className="mb-2 text-2xl xl:text3xl tracking-tight">
           <span>Web Development</span> <span>Done Right</span>
         </h2>
