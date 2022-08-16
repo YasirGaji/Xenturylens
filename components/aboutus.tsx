c0f0d4de7e02c00dc3f8d4bd67671aaf60ecb630
Yasir Gaji <gajiyasir@gmail.com>
2022-08-16 22:02:54 +0100

    added first set of about us section texts

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const AboutUs: React.FC = () => {
-  return <div>About US</div>;
+  return (
+    <section className={'flex flex-col bg-white py-20 text-3xl md:text-4xl'}>
+      <div className="container mx-auto px-11">
+        <p className="leading-light max-w-5xl mx-auto text-4xl tracking-tight">
+          <strong>We will help ship better softwares, faster.</strong> Our team
+          of professional engineers have created the best user experience in
+          some of the most popular softwares in the world
+        </p>
+      </div>
+      <div className="container mx-auto"></div>
+    </section>
+  );
 };
 
 export default AboutUs;
--- components/masthead.tsx
+++ components/masthead.tsx
@@ -25,7 +25,7 @@ const Masthead: React.FC = () => {
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">XenturyLens</h1>
         <h2 className="mb-2 text-2xl xl:text3xl tracking-tight">
-          <span>Web Development</span> <span>Done Right</span>
+          <span>Software Development</span> <span>Done Right</span>
         </h2>
       </div>
       <div className="flex-grow-0 pb-20 md:pb-10 transition-all duration-1000">
