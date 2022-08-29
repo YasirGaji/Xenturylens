08b641f5a23d34722cbc99a853cbc44c36adb000
Yasir Gaji <gajiyasir@gmail.com>
2022-08-29 23:19:37 +0100

    Alhamdulillah

--- components/skills.tsx
+++ components/skills.tsx
@@ -5,7 +5,15 @@ const Skills: React.FC = () => {
     <div className="bg-black text-white">
       <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl lg:text-7xl tracking-tight font-semibold">
         <div className="leading-[1.15]">We know Our Tools Inside Out</div>
-        <span>Our team has contributed to </span>
+        <span>
+          We are continiously contributing to the high tech product industry in
+          Africa, with the aim of powering thousands of businesses,
+          coorporations, and organisations
+        </span>
+        <span>
+          We&apos;re maintaining some of the most popular e-commerce platforms,
+          with over <strong>$5,784</strong> daily sales.
+        </span>
       </div>
     </div>
   );
