b75ffe0ce3c8779a20d89f8f54b5499833dcae4b
Yasir Gaji <gajiyasir@gmail.com>
2022-08-29 20:23:34 +0100

    looking better and more like it

--- components/skills.tsx
+++ components/skills.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Skills: React.FC = () => {
   return (
     <div className="bg-black text-white">
-      <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20">
+      <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl lg:text-7xl tracking-tight font-semibold">
         Skills
       </div>
     </div>
