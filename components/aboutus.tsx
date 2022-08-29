a4abe58af23b2cad48870916652e12ef9643387e
Yasir Gaji <gajiyasir@gmail.com>
2022-08-29 10:11:20 +0100

    Building members section

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -12,8 +12,13 @@ const AboutUs: React.FC = () => {
       </div>
       <div className="container mx-auto px-11 text-center mt-28">
         <h2>Our Team</h2>
-        <div>the &ldquo;spec-ops&rdquo;</div>
-        <div>
+        <div className="mt-2">the &ldquo;spec-ops&rdquo;</div>
+        <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-20">
+          <div>member</div>
+          <div>member</div>
+          <div>member</div>
+          <div>member</div>
+          <div>member</div>
           <div>member</div>
         </div>
       </div>
