34c1ed2a901f796c2c957d15f1eac602cb737193
Yasir Gaji <gajiyasir@gmail.com>
2022-09-02 16:06:34 +0100

    completed the projects component

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -13,7 +13,7 @@ const AboutUs: React.FC = () => {
       </div>
       <div className="container mx-auto px-11 text-center mt-28">
         <h2>Our Team</h2>
-        <div className="mt-2">the &ldquo;spec-ops&rdquo;</div>
+        <div className="mt-2">the &ldquo;sabi-boyz&rdquo;</div>
         <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-20">
           <Member
             id="Yasir"
