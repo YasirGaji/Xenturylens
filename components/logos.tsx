8c51c5b581ae06c1166eca025a8b1b9e7f9b3286
Yasir Gaji <gajiyasir@gmail.com>
2022-09-10 11:49:11 +0100

    reed

--- components/logos.tsx
+++ components/logos.tsx
@@ -7,7 +7,7 @@ const Logos: React.FC = () => {
     <>
       <SliderContainer initialOffsetX={0} contentWidth={1290} width={0}>
         <SliderItem initialOffsetX={0} contentWidth={0} width={150}>
-          <Image src="/assets/brands/logo.svg" alt="logo" width={150} height={50} objectFit="contain" />
+          <Image src="/assets/brands/bafsta.png" alt="logo" width={150} height={50} objectFit="contain" />
         </SliderItem>
 
         <SliderItem initialOffsetX={0} contentWidth={0} width={200}>
@@ -50,10 +50,6 @@ const Logos: React.FC = () => {
 
         <SliderItem initialOffsetX={0} contentWidth={0} width={350}>
           <Image src="/assets/brands/logo.svg" alt="logo" width={350} height={50} objectFit="contain" />
-        </SliderItem>
-
-        <SliderItem initialOffsetX={0} contentWidth={0} width={400}>
-          <Image src="/assets/brands/hs2.png" alt="Highstreet" width={400} height={50} objectFit="contain" />
         </SliderItem>
       </SliderContainer>
     </>
