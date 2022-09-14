014a27063672db4553ed14fa8f79bd509ac18ae8
Yasir Gaji <gajiyasir@gmail.com>
2022-09-14 10:39:47 +0100

    roses

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -8,6 +8,13 @@ const ContactUs: React.FC = () => {
         <Image src="/logo2.png" width={30} height={30} alt="logo" />
       </div>
       <h2 className="text-4l font-bold">Contact Us</h2>
+
+      <form className="flex flex-col gap-4 mt-16 px-10 lg:mt-20 min-w-full lg:min-w-[500px]">
+
+        <input id="companyName" name="companyName" type="text" placeholder="Company Name"  required maxLength={128} className="bg-black text-white border-2 border-white rounded-3xl outline-none p-2 mt-10 px-8 w-96" />
+
+        <input id="email" name="email" type="email" placeholder="Email" className="bg-black text-white border-2 border-white rounded-3xl outline-none p-2 mt-10 px-8 w-96 py-2" />
+      </form>
     </div>
   )
 }
