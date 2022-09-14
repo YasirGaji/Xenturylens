be9d7574e93a4525a3713c06115df003c863c65f
Yasir Gaji <gajiyasir@gmail.com>
2022-09-14 08:26:43 +0100

    added the logo

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 
 const ContactUs: React.FC = () => {
   return (
-    <div className="bg-black text-white flex flex-col justify-center pt-10 min-h-screen items-center pt-10">
+    <div className="bg-black text-white flex flex-col justify-center pt-10 min-h-screen items-center pb-10">
+      <div className="pb-10">
+        <Image src="/assets/logo.png" width={30} height={30} alt="logo" />
+      </div>
       <h2 className="text-4l font-bold">Contact Us</h2>
     </div>
   )
