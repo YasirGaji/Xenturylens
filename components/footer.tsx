fbb0efd227e38cd4c0317c9d7fe09fffd90aac49
Yasir Gaji <gajiyasir@gmail.com>
2022-09-15 08:45:56 +0100

    changed the padding

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,10 +6,10 @@ import Image from 'next/image';
 const Footer: React.FC = () => {
   return (
     <div>
-      <footer className='min-h-full flex gap-8 items-center justify-center bg-black text-white p-20'>
+      <footer className='min-h-full flex gap-8 items-center justify-center bg-black text-white p-10'>
         <Image src="/logo2.png" width={20} height={20} alt="logo" />
         <Link href="/terms">Terms</Link>
-        <Link href="/privacy">Privacy Policy</Link>
+        <Link href="/privacy">Policy Policy</Link>
       </footer>
     </div>
   )
