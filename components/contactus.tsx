f6423ba711fa7217989027f78af4c5e2e0de947f
Yasir Gaji <gajiyasir@gmail.com>
2022-09-15 11:59:07 +0100

    in

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -65,7 +65,7 @@ const ContactUs: React.FC = () => {
       })
       .catch((error) => {
         handleServerResponse(false, error.response.data.error)
-        console.log(error.response.data.error)
+        console.log(error)
       })
   }, [inputs, handleServerResponse])
 
